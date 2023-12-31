435cdd048bfb2c1fa3c8f1d6445def3269a484f9
Angel S. Moreno <angelxmoreno@gmail.com>
2023-12-31 18:15:14 -0500

    feat: created ClientApi

---
+++ src/classes/ClientApi.ts
@@ -0,0 +1,21 @@
+import RestFulApi, { EntityResponse, WithIdParams } from '../shared/RestFulApi';
+import { ClientEntity } from '../shared/entities';
+import { CreateClientPayload, PatchClientPayload } from '../shared/payloads';
+
+export class ClientApi extends RestFulApi<ClientEntity, CreateClientPayload, PatchClientPayload> {
+    uri = '/clients';
+
+    async clock({ jwt, id }: WithIdParams): Promise<EntityResponse<ClientEntity>> {
+        try {
+            const { data } = await this.http.post<ClientEntity>(`${this.uri}/${id}/clock`, {
+                headers: {
+                    Authorization: `Bearer ${jwt}`,
+                },
+            });
+
+            return { data, error: undefined };
+        } catch (e) {
+            return { data: undefined, error: e as Error };
+        }
+    }
+}
